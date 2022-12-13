ec8dac6fe5cc8516f4623ffb3e5dadb76b784e0d
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-12-13 07:31:34 -0300

    dev: create User EF Model

---
+++ FinalProjectCSharpTrybe/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinalProjectCSharpTrybe.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string? Module { get; set; }
+
+        [Required]
+        public int Status { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Password { get; set; }
+
+        public ICollection<Post>? Posts { get; set; }
+    }
+}
+
