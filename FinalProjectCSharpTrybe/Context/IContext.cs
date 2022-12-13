88b82bbf86d25f6fd4b1cc0c2d04180f1a632840
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-12-13 07:33:40 -0300

    dev: create IContext Interface

---
+++ FinalProjectCSharpTrybe/Context/IContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalProjectCSharpTrybe.Models;
+
+namespace FinalProjectCSharpTrybe.Context
+{
+    public interface IContext
+    {
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+
+        public int SaveChanges();
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
