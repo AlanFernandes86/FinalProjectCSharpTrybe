218d690117c7985015145447a87de972af1145c4
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-12-13 07:32:20 -0300

    dev: create IPostRepository interface

---
+++ FinalProjectCSharpTrybe/Repository/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using FinalProjectCSharpTrybe.Models;
+
+namespace FinalProjectCSharpTrybe.Repository
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetAllPosts(int userId);
+
+        Task<Post> GetLastPost(int userId);
+
+        int SetPost(Post post);
+        int DeletePost(Post post);
+        int UpdatePostMessage(int id, string message);
+    }
+}
