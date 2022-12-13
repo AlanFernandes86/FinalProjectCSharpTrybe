3049efe0166848208e6481d2d7891cb864ff50d4
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-12-13 07:32:31 -0300

    dev: create IUserRepository interface

---
+++ FinalProjectCSharpTrybe/Repository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using FinalProjectCSharpTrybe.Models;
+
+namespace FinalProjectCSharpTrybe.Repository
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserById(int userId);
+
+        Task<IEnumerable<User>> GetUsersByName(string name);
+
+        int SetUser(User user);
+        int DeleteUser(User user);
+        int UpdateUser(User user);
+    }
+}
+
