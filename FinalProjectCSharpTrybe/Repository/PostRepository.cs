ec388c2ccf865b79c02706a3e9db5d97c44616d7
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-12-13 07:33:06 -0300

    dev: create PostRepository Implementation

---
+++ FinalProjectCSharpTrybe/Repository/PostRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalProjectCSharpTrybe.Context;
+using FinalProjectCSharpTrybe.Models;
+
+namespace FinalProjectCSharpTrybe.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly IContext _context;
+        public PostRepository(IContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Post>> GetAllPosts(int userId)
+        {
+            return await _context.Posts.Where((post) => post.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Post> GetLastPost(int userId)
+        {
+            return await _context.Posts.Where((post) => post.UserId == userId).OrderByDescending(p => p.UpdatedAt).FirstAsync();
+        }
+        public int SetPost(Post post)
+        {
+            _context.Posts.Add(post);
+            return _context.SaveChanges();
+        }
+
+        public int DeletePost(Post post)
+        {
+            _context.Posts.Remove(post);
+            return _context.SaveChanges();
+        }
+
+        public int UpdatePostMessage(int id, string message)
+        {
+            return _context.Posts.Where(x => x.Id == id).ExecuteUpdate((y) => y.SetProperty((post) => post.Message, message));
+
+            //var post = new Post() { Id = id, Message = message };
+
+            //_context.Posts.Attach(post);
+            //_context.Posts.Entry(post).Property(x => x.Message).IsModified = true;
+            //return _context.SaveChanges();
+        }
+    }
+
+}
