4f7edf9c2f6e2ee1cb2e86cbf832e7aced8993cc
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-12-13 07:31:43 -0300

    dev: create Post EF Model

---
+++ FinalProjectCSharpTrybe/Models/Post.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace FinalProjectCSharpTrybe.Models
+{
+    [Table("Posts")]
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string? Title { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string? Message { get; set; }
+
+        public string? ImageURL { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public int UserId { get; set; }
+
+        [JsonIgnore]
+        public User? User { get; set; }
+    }
+}
+
